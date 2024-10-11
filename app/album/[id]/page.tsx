3de4a168f2222f5197d915ced46e847d9a7f18ef
avoidme12 <zaharovds10@gmail.com>
2024-10-11 20:26:41 +1000

    upd assets

---
+++ app/album/[id]/page.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+
+import Sidebar from "@/components/Sidebar";
+import Display from "@/components/Display";
+import {BrowserRouter} from "react-router-dom";
+
+export default function AlbumPage(){
+    return(
+        <BrowserRouter>
+            <div className="h-screen bg-black">
+                <div className='h-[90%] flex'>
+                    <Sidebar/>
+                    <Display/>
+                </div>
+            </div>
+        </BrowserRouter>
+    )
+}
